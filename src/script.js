e45ec64a5ae21bd8164c3a2cac4603dfeb7e2881
AnzhelikaKazanok <106910002+AnzhelikaKazanok@users.noreply.github.com>
2022-07-07 13:49:00 +0200

    relevant main icon

--- src/script.js
+++ src/script.js
@@ -45,10 +45,49 @@ function displayTemperature(response) {
   let cityElement = document.querySelector("#city");
   let humidityElement = document.querySelector("#humidity");
   let windElement = document.querySelector("#wind");
+  let iconElementAlt = document.querySelector("#icon");
+
   temperatureElement.innerHTML = Math.round(response.data.main.temp);
   cityElement.innerHTML = response.data.name;
   humidityElement.innerHTML = response.data.main.humidity;
   windElement.innerHTML = Math.round(response.data.wind.speed);
+  iconElementAlt.setAttribute("alt", response.data.weather[0].description);
+
+  let mainIcon = response.data.weather[0].icon;
+  if (mainIcon === "01d" || mainIcon === "01n") {
+    document.querySelector("#icon").setAttribute("src", "images/sun-60.png");
+  }
+  if (
+    mainIcon === "02d" ||
+    mainIcon === "02n" ||
+    mainIcon === "03d" ||
+    mainIcon === "03n" ||
+    mainIcon === "04d" ||
+    mainIcon === "04n"
+  ) {
+    document.querySelector("#icon").setAttribute("src", "images/cloud-60.png");
+  }
+  if (
+    mainIcon === "09d" ||
+    mainIcon === "09n" ||
+    mainIcon === "10d" ||
+    mainIcon === "10n"
+  ) {
+    document
+      .querySelector("#icon")
+      .setAttribute("src", "images/heavy-rain-60.png");
+  }
+  if (mainIcon === "11d" || mainIcon === "11n") {
+    document.querySelector("#icon").setAttribute("src", "images/storm-60.png");
+  }
+  if (mainIcon === "13d" || mainIcon === "13n") {
+    document
+      .querySelector("#icon")
+      .setAttribute("src", "images/snowy-winter-60.png");
+  }
+  if (mainIcon === "50d" || mainIcon === "50n") {
+    document.querySelector("#icon").setAttribute("src", "images/fog-60.png");
+  }
 }
 
 let apiKey = "3959d341d5294447fa3d604e983c20c9";
