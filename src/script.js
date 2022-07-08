f184e9285ce3b3425dbeb9222df40afe5dfac81a
AnzhelikaKazanok <106910002+AnzhelikaKazanok@users.noreply.github.com>
2022-07-08 18:42:39 +0200

    display the forecast data using api

--- src/script.js
+++ src/script.js
@@ -40,27 +40,43 @@ dayNow.innerHTML = `${currentDay}`;
 dateNow.innerHTML = `${currentMonth}, ${currentDate}th`;
 timeNow.innerHTML = `${currentHour}:${currentMinute}`;
 //
+function formatDay(timestamp) {
+  let date = new Date(timestamp * 1000);
+  let day = date.getDay();
+  let days = ["Sun", "Mon", "Tue", "Wed", "Thu", "Fri", "Sat"];
+
+  return days[day];
+}
+//
 function displayForecast(response) {
-  console.log(response.data.daily);
+  let forecast = response.data.daily;
+
   let forecastElement = document.querySelector("#forecast");
 
   let forecastHTML = `<div class="row">`;
-  let days = ["Sat", "Sun", "Mon", "Tue", "Wed"];
-  days.forEach(function (day) {
-    forecastHTML =
-      forecastHTML +
-      ` 
+  forecast.forEach(function (forecastDay, index) {
+    if (index < 5) {
+      forecastHTML =
+        forecastHTML +
+        ` 
             <div class="col">
               <div class="weather-forecast-block">
-                <div class="weather-forecast-date">${day}</div>
+                <div class="weather-forecast-date">${formatDay(
+                  forecastDay.dt
+                )}</div>${index}
                 <img src="images/heavy-rain-60.png" height="30" alt="" />
                 <div class="weather-forecast-temperature">
-                  <span class="weather-forecast-temperature-max"> 30° </span>
-                  <span class="weather-forecast-temperature-min"> 20° </span>
+                  <span class="weather-forecast-temperature-max"> ${Math.round(
+                    forecastDay.temp.max
+                  )}° </span>
+                  <span class="weather-forecast-temperature-min"> ${Math.round(
+                    forecastDay.temp.min
+                  )}° </span>
                 </div>
               </div>
             </div>
           `;
+    }
   });
 
   forecastHTML = forecastHTML + `</div>`;
@@ -68,7 +84,6 @@ function displayForecast(response) {
 }
 //
 function getForecast(coordinates) {
-  console.log(coordinates);
   let apiKey = "3959d341d5294447fa3d604e983c20c9";
   let apiUrl = `https://api.openweathermap.org/data/2.5/onecall?lat=${coordinates.lat}&lon=${coordinates.lon}&appid=${apiKey}&units=metric`;
   axios.get(apiUrl).then(displayForecast);
